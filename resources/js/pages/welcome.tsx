1c227d0c0ea264e2d4fce5dfd07b87ae304db881
JsemOlik <github@jsemolik.dev>
2026-03-20 10:05:22 +0100

    changes

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -274,7 +274,7 @@ export default function Welcome() {
                                                                                         Short link
                                                                                     </div>
                                                                                     <div className="mt-2 flex items-center justify-between rounded-xl bg-[#f53003]/8 px-3 py-2 text-sm font-semibold text-[#f53003] dark:bg-[#ff4433]/10 dark:text-[#ff7b6d]">
-                                                                                        <span>bfw.cz/link/demo</span>
+                                                                                        <span>bfw.cz/demo</span>
                                                                                         <span className="text-[11px] tracking-[0.16em] uppercase">
                                                                                             Ready
                                                                                         </span>
