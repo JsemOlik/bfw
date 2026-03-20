55f18bd860ba3bb94bdcc1176119739fbd8e934e
Oliver Steiner <github@jsemolik.dev>
2026-03-20 20:25:56 +0100

    ads.txt

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="google-adsense-account" content="ca-pub-8327290007131872">
-
     {{-- Inline script to detect system dark mode preference and apply it immediately --}}
     <script>
         (function () {
